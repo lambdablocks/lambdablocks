03737c5e1d2c9249a1cae9c926f509e442fe64dc
Matthieu Caneill <matthieu.caneill@imag.fr>
2017-03-17 17:02:56 +0100

    add args parsing to entry point, fix example

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 * python3
 * python3-yaml
+
+## Run examples
+
+```
+source env.sh
+python3 bin/blocks.py -f examples/wordcount.yml
+```
--- bin/blocks.py
+++ bin/blocks.py
@@ -1,7 +1,21 @@
-from lb.graph import compute_graph
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Runs a yaml-defined DAG.')
+    parser.add_argument('-f', '--filename',
+                        required=True,
+                        help='YAML file to be executed.')
+    args = parser.parse_args()
+    return args
 
 def main():
-    compute_graph('examples/foo.yml')
+    args = parse_args()
+
+    # imported here to avoid creating a Spark context when not needed
+    from lb.graph import compute_graph
+
+    compute_graph(args.filename)
 
 if __name__ == '__main__':
     main()
