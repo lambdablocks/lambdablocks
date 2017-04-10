3bcdd803d73d935e813789b6ced27ed43f589472
Matthieu Caneill <matthieu.caneill@imag.fr>
2017-04-10 18:10:24 +0200

    implement multi output

--- lb/graph.py
+++ lb/graph.py
@@ -31,11 +31,25 @@ def build_graph(blocks):
             # we still want to have an empty list to avoid KeyErrors
             block['inputs'] = []
 
-    edges = defaultdict(list)
-    for k,v in vertices.items():
-        if 'inputs' in v.keys():
-            for input_ in v['inputs']:
-                edges[input_].append(k)
+    edges = defaultdict(lambda: {'prev': [],
+                                 'next': []})
+
+    for block2, block2_props in vertices.items():
+        for input_ in block2_props['inputs']:
+            # we add the vertice between block1 and block2, in both directions
+            if '.' in input_:
+                # named output, when a block outputs multiple values
+                try:
+                    block1, value = input_.split('.')
+                except ValueError as e:
+                    print('Wrong input: ' + input_)
+                    raise e
+            else:
+                # the block outputs a single value
+                block1, value = input_, None
+
+            edges[block1]['next'].append({'name': block2, 'value': value})
+            edges[block2]['prev'].append({'name': block1, 'value': value})
 
     return entry_points, vertices, edges
 
@@ -52,8 +66,8 @@ def execute(inputs, vertices, edges):
     while len(fun_queue) > 0:
         block_name = fun_queue.popleft()
         # do the block inputs have been computed yet?
-        for input_ in vertices[block_name]['inputs']:
-            if input_ not in results.keys():
+        for input_ in edges[block_name]['prev']:
+            if input_['name'] not in results.keys():
                 fun_queue.append(block_name)
                 break
         else: # ok, all inputs have been computed, we proceed
@@ -62,17 +76,26 @@ def execute(inputs, vertices, edges):
                 comp_args = vertices[block_name]['args']
             except KeyError:
                 comp_args = {}
-            try:
-                comp_inputs = [results[x] for x in vertices[block_name]['inputs']]
-            except KeyError as e:
-                raise Exception('%s was scheduled for execution, but lacks some inputs: %s'
-                                % (block_name, str(vertices[block_name]['inputs'])))
+
+            # we prepare the block's input values
+            comp_inputs = []
+            for input_ in edges[block_name]['prev']:
+                if input_['value'] is None:
+                    # no named value, single output from previous block
+                    get_input = lambda results: results[input_['name']]
+                else:
+                    get_input = lambda results: results[input_['name']][input_['value']]
+                try:
+                    comp_inputs.append(get_input(results))
+                except KeyError as e:
+                    raise Exception('%s was scheduled for execution, but lacks some inputs: %s'
+                                    % (block_name, str(vertices[block_name]['inputs'])))
             results[block_name] = comp_fun(**comp_args)(*comp_inputs)
             # we add this block's destinations to the queue,
             # if they are not there already
-            for destination in edges[block_name]:
-                if destination not in fun_queue:
-                    fun_queue.append(destination)
+            for destination in edges[block_name]['next']:
+                if destination['name'] not in fun_queue:
+                    fun_queue.append(destination['name'])
 
 def compute_graph(filename):
     blocks = parse_yaml(filename)
