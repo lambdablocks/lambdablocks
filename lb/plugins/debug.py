65f797869a161da19c1e192d27cbd4e70ff9c7f6
Matthieu Caneill <matthieu.caneill@imag.fr>
2017-09-14 14:48:45 +0200

    add plugin: debug, which prints an excerpt of every block result

---
+++ lb/plugins/debug.py
@@ -0,0 +1,54 @@
+# Copyright 2017 The Lambda-blocks developers. See AUTHORS for details.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Debug plugin, prints an excerpt of each block's result after its
+execution.
+"""
+
+import sys
+
+from lb.plugins_manager import after_block_execution
+
+def shorten(item, length=100):
+    s = repr(item)
+    length = min(length, len(s))
+    result = s[:length]
+    if length < len(s):
+        result += '…'
+    return result
+
+@after_block_execution
+def show_current_data(block, results):
+    print('For block {}'.format(block.fields['name']))
+    if results:
+        for field, result in results._asdict().items():
+            print('  {}'.format(field))
+            if isinstance(result, list):
+                sys.stdout.write('    [')
+                if isinstance(result[0], list):
+                    sys.stdout.write('[')
+                    if isinstance(result[0][0], list):
+                        sys.stdout.write("That's quite a nested list (FIXME)")
+                    else:
+                        sys.stdout.write('{}'.format(shorten(result[0][0])))
+                    sys.stdout.write(']')
+                else:
+                    sys.stdout.write('{}'.format(shorten(result[0])))
+                sys.stdout.write(']')
+            else:
+                print('{}'.format(shorten(result)))
+            sys.stdout.write('\n')
+    else:
+        print('  No results for this block.')
