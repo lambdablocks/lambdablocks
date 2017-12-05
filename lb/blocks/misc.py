8c142184e8f4b746abbd482ce3e10e1a799ed19c
Matthieu Caneill <matthieu.caneill@imag.fr>
2017-12-05 18:18:06 +0100

    doc: properly document misc

--- lb/blocks/misc.py
+++ lb/blocks/misc.py
@@ -24,8 +24,9 @@ from lb.utils import ReturnEntry, default_function
 
 @block(engine='all')
 def show_console():
-    """
-    Pretty prints in green on the console.
+    """Pretty prints in green on the console.
+
+    :input Any data: The field you want to display.
     """
     def inner(data: Any):
         print('\033[92m') # green
@@ -35,6 +36,11 @@ def show_console():
 
 @block(engine='all')
 def write_line(filename: str=''):
+    """Writes a string in a file.
+
+    :param str filename: The file you want to write to.
+    :input str data: The value to write in the file.
+    """
     def inner(data: str):
         with open(filename, 'w') as f:
             f.write(data)
@@ -42,6 +48,11 @@ def write_line(filename: str=''):
 
 @block(engine='all')
 def write_lines(filename: str=''):
+    """Writes strings in a file, all on a new line.
+
+    :param str filename: The file you want to write to.
+    :input List[str] data: The values to write in the file.
+    """
     def inner(data: List[str]):
         with open(filename, 'w') as f:
             for line in data:
@@ -51,27 +62,50 @@ def write_lines(filename: str=''):
 
 @block(engine='all')
 def split(sep: str='\n'):
+    """Splits a string.
+
+    :param str sep: The separator on which to split.
+    :input str data: The string you want to split.
+    :output List[str] result: The list of splits.
+    """
     def inner(data: str) -> ReturnType[List[str]]:
         return ReturnEntry(result=data.split(sep))
     return inner
 
 @block(engine='all')
 def concatenate(sep: str='\n'):
+    """Joins a list of strings.
+
+    :param str sep: What will separate the strings in the result.
+    :input List[Any] data: The list of items you want to join.
+    :output Any result: The joined result.
+    """
     def inner(data: List[Any]) -> ReturnType[Any]:
         return ReturnEntry(result=sep.join(data))
     return inner
 
 @block(engine='all')
 def map_list(func: Callable[[Any], Any]=default_function(1)):
+    """Applies a function to every element of a list and returns the
+    resulting list.
+
+    :param Callable func: The function to apply.
+    :input List[Any] data: The input list.
+    :output List[Any] result: The mapped list.
+    """
     def inner(data: List[Any]) -> ReturnType[List[Any]]:
         return ReturnEntry(result=list(map(func, data)))
     return inner
 
 @block(engine='all')
 def flatMap(func: Callable[[Any], List[Any]]=default_function(1, [])):
-    """
-    Applies a map function to every item, and then flattens the result.
-    [a,b,c] -> [[x,y],[z],[]] -> [x,y,z]
+    """Applies a map function to every item, and then flattens the result.
+
+    ``[a,b,c] -> [[x,y],[z],[]] -> [x,y,z]``
+
+    :param Callable func: The function to apply.
+    :input List[Any] data: The input list.
+    :output List[Any] result: The mapped list.
     """
     def inner(data: List[Any]) -> ReturnType[List[Any]]:
         result = []
@@ -82,6 +116,13 @@ def flatMap(func: Callable[[Any], List[Any]]=default_function(1, [])):
 
 @block(engine='all')
 def flatten_list():
+    """Flattens a list of lists.
+
+    ``[[a,b],[c,d]] -> [a,b,c,d]``
+
+    :input List[List[Any]] data: The list to flatten.
+    :output List[Any] result: The flattened list.
+    """
     def inner(data: List[List[Any]]) -> ReturnType[List[Any]]:
         result = []
         for item in data:
@@ -91,8 +132,10 @@ def flatten_list():
 
 @block(engine='all')
 def group_by_count():
-    """
-    Groups items in a similar manner as SQL's `COUNT()…GROUP BY()`.
+    """Groups items in a similar manner as SQL's ``COUNT()…GROUP BY()``.
+
+    :input List[Any] data: The list to group and count.
+    :output List[Tuple[Any,int]] result: A list of tuples (item, count).
     """
     def inner(data: List[Any]) -> ReturnType[List[Tuple[Any,int]]]:
         grouped = itertools.groupby(sorted(data))
@@ -103,6 +146,13 @@ def group_by_count():
 @block(engine='unixlike')
 def sort(key: Callable[[Any], Any]=default_function(1),
          reverse: bool=False):
+    """Sorts a list.
+
+    :param Callable key: A function to select the element to sort on, similar to Python's key argument to list.sort.
+    :param bool reverse: Set to true to return the inverted result.
+    :input List[Any] data: The list to sort.
+    :output List[Any] result: The sorted list.
+    """
     def inner(data: List[Any]) -> ReturnType[List[Any]]:
         result = sorted(data, key=key, reverse=reverse)
         return ReturnEntry(result=result)
